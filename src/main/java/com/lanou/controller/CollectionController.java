8bb14623d4e957939a9e5e3c8194aa9c6941fd19
WacngChangLin <31278777+ZhiZunBao0811@users.noreply.github.com>
2017-12-05 22:06:10 +0800

    day4wcl
    FastJson

--- src/main/java/com/lanou/controller/CollectionController.java
+++ src/main/java/com/lanou/controller/CollectionController.java
@@ -2,12 +2,14 @@ package com.lanou.controller;
 
 import com.lanou.entity.Goods;
 import com.lanou.service.CollectionService;
+import com.lanou.util.FastJson_All;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.util.List;
 
@@ -22,10 +24,10 @@ public class CollectionController {
 
     //通过用户Id查询收藏的商品(查看收藏夹)
     @RequestMapping("/findGoodsList.do")
-    @ResponseBody
-    public List<Goods> findGoodsList(Integer uId){
+//    @ResponseBody
+    public void findGoodsList(Integer uId,HttpServletResponse response){
           List<Goods> goodsList = collectionService.findGoodsList(uId);
-          return goodsList;
+          FastJson_All.toJson(goodsList,response);
     }
 
 //    @RequestMapping("/findRepeat")
@@ -39,35 +41,37 @@ public class CollectionController {
 
     //收藏商品
     @RequestMapping("/addCollection.do")
-    @ResponseBody
-    public int addCollection(Integer uId, Integer gId, HttpServletRequest request){
+//    @ResponseBody
+    public void addCollection(Integer uId, Integer gId, HttpServletRequest request,HttpServletResponse response){
         HttpSession session =  request.getSession();
+        int num = 0;
         if (session==null){
 //          String login = "请登录";
-            return 1;
+            num = 1;
         }
         Integer result = collectionService.findRepeat(uId, gId);
         if (result==null){
             boolean results = collectionService.addCollection(uId, gId);
             if (results){
 //                String OK = "商品收藏成功!";
-                return 2;
+                num =2;
             }
         }
 //        String NO= "您已经收藏过了!";
-        return 0;
+        FastJson_All.toJson(num,response);
     }
 
     @RequestMapping("/deleteCollection.do")
-    @ResponseBody
-    public boolean delete(Integer uId, Integer gId){
+//    @ResponseBody
+    public void delete(Integer uId, Integer gId,HttpServletResponse response){
         boolean result = collectionService.deleteCollection(uId, gId);
+        boolean res = false;
         if (result){
             //删除成功
-            return true;
+            res = true;
         }
         //删除失败
-        return false;
+        FastJson_All.toJson(res,response);
     }
 
 }
