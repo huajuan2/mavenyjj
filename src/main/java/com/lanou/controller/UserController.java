962a676052b06b01e35aed64ebf09bf0ca3aaea6
WacngChangLin <31278777+ZhiZunBao0811@users.noreply.github.com>
2017-12-06 17:00:16 +0800

    day05wcl
    获取Session的方法

--- src/main/java/com/lanou/controller/UserController.java
+++ src/main/java/com/lanou/controller/UserController.java
@@ -151,8 +151,6 @@ public class UserController {
 	//返回Session
 	@RequestMapping("/getSession")
 	public void Getsession(HttpServletRequest request,HttpServletResponse response){
-		User user = new User(1,"wcl");
-		request.getSession().setAttribute("user",user);
 		FastJson_All.toJson(request.getSession().getAttribute("user"),response);
 	}
 }
